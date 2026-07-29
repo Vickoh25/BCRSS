9803edfa56193906d2829ab4e13d1f38a4154428
Vickoh25 <okellov911@gmail.com>
2026-07-29 09:33:13 +0000

    Deploying to gh-pages from @ Vickoh25/BCRSS@d602ef585de85856a01f5d92ec3809df23d0578d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1175251921"
+    serviceWorkerVersion: "2654766614"
   }
 });
