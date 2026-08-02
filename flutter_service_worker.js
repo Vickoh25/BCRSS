ce21101be4b974f51465bbf1f8fc262b93e59997
Vickoh25 <okellov911@gmail.com>
2026-08-02 10:15:28 +0000

    Deploying to gh-pages from @ Vickoh25/BCRSS@daa878df6a37bcdcfceb4765178269c316dd10ca 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "f81e96fa7e19e388464c1b6fbd8b1385",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+const RESOURCES = {"favicon.png": "c953fa51fb119952ff4d25cab3f0bad2",
+"flutter_bootstrap.js": "2523ef12ce7a339fb4cd02f0d1752fa1",
+"icons/Icon-192.png": "6033298c347f88be175f767ab441d79e",
+"icons/Icon-512.png": "b8befe31dd8fee9d25aebbdf72edeb48",
+"icons/Icon-maskable-512.png": "b8befe31dd8fee9d25aebbdf72edeb48",
+"icons/Icon-maskable-192.png": "6033298c347f88be175f767ab441d79e",
 "index.html": "573af31acdd83bfc4e9ee17a3d256303",
 "/": "573af31acdd83bfc4e9ee17a3d256303",
 "main.dart.js": "b317e9ba2e5c18da46e12cf5003c7852",
