1a909eba3528e530d57d6a03097caba0f0c59476
Victor Okello <okellov911@gmail.com>
2026-08-06 11:17:47 +0300

    Update settings.py

--- backend/bcrss_config/settings.py
+++ backend/bcrss_config/settings.py
@@ -148,7 +148,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
-    'http://localhost,http://127.0.0.1,http://localhost:80,http://127.0.0.1:80,http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000,http://localhost:8080,http://127.0.0.1:8080,https://bcrss.vercel.app'
+    'http://localhost,http://127.0.0.1,http://localhost:80,http://127.0.0.1:80,http://localhost:3000,http://127.0.0.1:3000,http://localhost:8000,http://127.0.0.1:8000,http://localhost:8080,http://127.0.0.1:8080,https://bcrss-6mxrxtkga-bcrss.vercel.app,https://bcrss.vercel.app'
 ).split(',')
 CORS_ALLOW_ALL_ORIGINS = os.getenv('CORS_ALLOW_ALL_ORIGINS', 'False') == 'True'
 
