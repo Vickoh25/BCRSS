bf23528696c81a0924a357faccf401f7246a6acf
Victor Okello <okellov911@gmail.com>
2026-08-09 23:45:56 +0300

    Update views.py

--- backend/borrow_requests/views.py
+++ backend/borrow_requests/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+import uuid
 from .models import BorrowRequest
 from .serializers import BorrowRequestSerializer, BorrowRequestCreateSerializer
 from resources.models import Resource
